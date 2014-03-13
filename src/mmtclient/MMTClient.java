c1e547fcb73a44fbc0e869e2fc0225df7b2d6456
phillip.johnson <phillip.johnson@172.16.217.142>
2014-03-13 11:14:01 -0500

    Start

---
+++ src/mmtclient/MMTClient.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mmtclient;
+
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author harlan.howe
+ */
+public class MMTClient extends JFrame{
+
+    public MMTClient()
+    {
+        super ("MMT");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,800);
+        getContentPane().setLayout(new GridLayout(1,1));
+        MMTGamePanel thePanel = new MMTGamePanel();
+        getContentPane().add(thePanel);
+        this.addKeyListener(thePanel);
+        setVisible(true);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MMTClient theApp = new MMTClient();
+    }
+    
+}
---
+++ src/mmtclient/MMTGamePanel.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mmtclient;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author harlan.howe
+ */
+public class MMTGamePanel extends JPanel implements KeyListener
+{
+    private boolean AisDown,SisDown,DisDown,WisDown;
+    
+    public MMTGamePanel()
+    {
+        super();
+        AisDown = false;
+        SisDown = false;
+        DisDown = false;
+        WisDown = false;
+    }
+    
+    
+    /** 
+     * detects when a key is pressed AND released. One of the required methods
+     * in the KeyListener interface.
+     * @param e 
+     */
+    @Override
+    public void keyTyped(KeyEvent e) {
+        ;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * detects when a key is first pressed. One of the required methods in the
+     * KeyListener interface.
+     * @param e 
+     */
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyChar()=='a') // notice... lower case, and this method only
+            AisDown = true;      //    works with alphanumeric keys. If you 
+        if (e.getKeyChar()=='s') //    want the arrow keys, you'll need to use 
+            SisDown = true;      //    getKeyCode() instead. (See API.)
+        if (e.getKeyChar()=='d')
+            DisDown = true;
+        if (e.getKeyChar()=='w')
+            WisDown = true;
+       
+        
+       
+    }
+    /**
+     * detects when a key is let up by the user. One of the required methods
+     * for the KeyListener interface.
+     * @param e 
+     */
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyChar()=='a')
+            AisDown = false;
+        if (e.getKeyChar()=='s')
+            SisDown = false;
+        if (e.getKeyChar()=='d')
+            DisDown = false;
+        if (e.getKeyChar()=='w')
+            WisDown = false;
+        
+        
+    }
+    
+}
